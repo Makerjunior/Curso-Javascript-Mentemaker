b5343fb169d3d9deb55f81e348ca927dbdc08677
Makerjunior <92755114+Makerjunior@users.noreply.github.com>
2023-07-19 02:24:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var nome = "Mente maker";
+nome = "Junior";
+nome = 5.6;
+const n = 3;
+// console.log(n + 6);
+//*******************************************
+var idade = 'a';
+const i = 'b';
+
+if (idade == i) {
+  console.log("A idade é iqual 30 .");
+} else {
+  console.log("A idade não é iqual 30 .");
+}
