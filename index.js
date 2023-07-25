ab8ccdcd1989bce2d30509bb8be4475b68fc5d2b
Junior Pereira <92755114+Makerjunior@users.noreply.github.com>
2023-07-24 21:53:04 -0300

    index.js

--- index.js
+++ index.js
@@ -4,11 +4,124 @@ nome = 5.6;
 const n = 3;
 // console.log(n + 6);
 //*******************************************
-var idade = 'a';
-const i = 'b';
 
-if (idade == i) {
-  console.log("A idade é iqual 30 .");
+// Exemplos de variáveis de diferentes tipos
+let numero = 42;
+console.log(numero);
+console.log(typeof numero);
+
+let texto = "Olá, mundo!";
+console.log(texto);
+console.log(typeof texto);
+
+let array = [
+  "Mente Maker",
+  134,
+  false,
+  { n: "junior" }
+];
+
+console.table(array[1]);
+console.log(typeof array);
+
+
+
+let objeto = {
+  chave: "valor",
+  a: "Maker",
+  b: 189,
+  c: false
+
+};
+console.table(objeto.c);
+console.log(typeof objeto);
+
+let booleano = false;
+console.log(booleano);
+console.log(typeof booleano);
+
+
+
+let nulo = null;
+console.log(nulo);
+//console.log(typeof nulo);
+
+let indefinido;
+console.log(indefinido)
+//console.log(typeof indefinido);
+
+
+
+
+
+
+var a = 1;
+const b = 1;
+
+// Iqual
+if (a === b) {
+  console.log("Verdeiro .");
 } else {
-  console.log("A idade não é iqual 30 .");
+  console.log("Falso .");
 }
+
+const n1 = 30;
+const n2 = 30;
+
+// Diferente
+if (n1 != n2) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+// Menor
+const n3 = 3;
+const n4 = 3;
+
+if (n3 <= n4) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+
+
+
+// Maior 
+
+const n5 = 2;
+const n6 = 2;
+
+if (n5 >= n6) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+// Menor iqual
+const n8 = 1;
+const n9 = 2;
+
+if (n8 <= n9) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+
+
+// Maior iqual
+const n10 = 1;
+const n11 = 2;
+
+if (n1 => n2) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+
+
+
+
