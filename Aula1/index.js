26bc4e111eb0d928b9e0a11730a6ce7166357106
Makerjunior <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>
2023-07-31 19:19:36 -0300

    Aula 3

---
+++ Aula1/index.js
@@ -0,0 +1,24 @@
+// Variáveis comuns
+var varivelComum = 10;
+console.log("Variável comum:", varivelComum);
+
+varivelComum = 20; // É possível alterar o valor da variável comum
+console.log("Variável comum atualizada:", varivelComum);
+
+
+//  Constantes
+const variavelConstante = 30;
+console.log("Variável constante:", variavelConstante);
+
+// variavelConstante = 40; // Gera um erro, pois não é possível alterar o valor de uma constante
+console.log("Variável constante atualizada:", variavelConstante);
+
+
+
+const idade = 18; // Declaração da variável constante 'idade' com o valor 18
+
+if (idade == 18) { // Verifica se a idade é maior ou igual a 18
+    console.log("Você é maior de idade."); // Imprime uma mensagem caso seja verdadeiro
+} else {
+    console.log("Você é menor de idade."); // Imprime uma mensagem caso seja falso
+}
---
+++ Aula3/index.js
@@ -0,0 +1,110 @@
+
+//   ###Aula 3
+
+// Módulo
+
+let c1 = 4;
+if (c1 % 2 == 0) {
+  console.log("O número :" + c1 + " é par !");
+} else {
+  console.log("O número :" + c1 + " é impar !");
+}
+
+console.log(8 % 3);
+
+
+//  Operadores logísticos
+// AND OR NOT
+
+const a2 = 2;
+const b2 = 2;
+// AND
+// a1 iqual b1 e 3 menor que 5
+
+if ((a2 == b2) && (3 < 5)) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+
+// OR
+// a1 iqual b1 ou 3 menor que 5
+const a3 = 2;
+const b3 = 2;
+if ((a3 == b3) || (10 < 5)) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+
+
+// NOT
+const a4 = 2;
+const b4 = 2;
+const x = 3;
+// a1 iqual b1 ou x Não é iqual 30
+if ((a4 == b4) && !(x == 30)) {
+  //console.log("Verdadeiro");
+} else {
+ // console.log("Falso");
+}
+
+let n= false;
+console.log(!n);
+
+
+// else if 
+
+let carro = "Opala";
+let motorista = "Paulo";
+
+if ((carro == "Marea") && (motorista == "Junior")) {
+  console.log(" Condição 1 ");
+  console.log(motorista + " tem um " + carro);
+}
+else if ((carro == "Marea") || (motorista == "Junior")) {
+  console.log(" Condição 2 ");
+  console.log(motorista + " tem um " + carro);
+}
+else {
+  console.log(" Condição 3 ");
+  console.log(motorista + " só anda a pé ");
+}
+
+
+// Operador ternario
+let x1 = 2;
+let y1 = 2;
+//(x1 != y1) ? console.log("Verdadeiro") : console.log("Falso");
+
+const res = (1 == 3) ? 4 + 4 : 8 * 2;
+console.log(res);
+
+
+
+// swith case 
+
+let diaDaSemana = 3;
+
+switch (diaDaSemana) {
+  case "segunda":
+    console.log("Hoje é segunda-feira");
+    break;
+  case 2:
+    console.log("Hoje é terça-feira");
+    break;
+  case 3:
+    console.log("Hoje é quarta-feira");
+    break;
+  case 4:
+    console.log("Hoje é quinta-feira");
+    break;
+  case 5:
+    console.log("Hoje é sexta-feira");
+    break;
+  default:
+    console.log("Dia inválido");
+}
+
