949626f32a088bb5283fd0cf2a021567933108a2
Makerjunior <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>
2023-09-25 20:18:57 -0300

    Aula9

--- Aula9/index.js
+++ Aula9/index.js
@@ -1,4 +1,31 @@
-const fruits = ['apple', 'banana', 'orange'];
-fruits.forEach((fruit) => {
-  console.log(fruit);
+
+
+
+
+let num =[0,1,2,3,4,5,6,7,8,9];
+
+num.forEach((n)=>{
+  if (n == 5) {
+    //console.log("Elnento = "+n);
+  }
 });
+
+
+let num2 =[0,1,2,3,4,5,6,7,8,9];
+
+let dobrado = num.map(function(a){
+  return a*2;   
+});
+
+
+
+console.log(dobrado);
+
+
+
+
+
+
+
+
+
