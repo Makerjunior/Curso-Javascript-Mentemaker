1fa0d9f270a693523690f891150e0f28e50bd422
Mekerjunior <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>
2024-06-24 19:10:14 -0300

    Aula18

--- Aula18/script.js
+++ Aula18/script.js
@@ -1,25 +1,17 @@
-// Seleciona o contêiner onde os novos elementos serão adicionados
-const container = document.getElementById('container');
 
-// Cria um novo elemento <p>
-const paragrafo = document.createElement('h1');
 
-// Define o conteúdo do parágrafo
-paragrafo.textContent = 'Este é um novo parágrafo criado com JavaScript!';
-
-// Adiciona o novo parágrafo ao contêiner
-container.appendChild(paragrafo);
-
-// Fucnção que cria um paragrafo
-function criarParagrafo(tag, texto, container) {
-    const T = document.createElement(tag);
-    T.textContent = texto; 
-    T.setAttribute("class", "txt");  // Adicionando atributo
-    container.appendChild(T);
+function criarParagrafo(tag, texto, containerId, classe) {
+    const conatiner = document.getElementById(containerId);
+    if (conatiner) {
+        const elementoTag = document.createElement(tag);
+        elementoTag.textContent = texto;
+        elementoTag.setAttribute('class',classe);
+        conatiner.appendChild(elementoTag);
+    }else{
+        console.error(`Não existe um container com id = ${conatiner}`);
     }
-    criarParagrafo('h1', 'Teste de Div', container);
-
-for (let index = 0; index < 5 ; index++) {
-    
-    criarParagrafo('h1', 'Teste de Div', container);
 }
+
+criarParagrafo('h1','Mente Maker','container','txt');
+criarParagrafo('h1','Mente Maker','container','txt1');
+criarParagrafo('h1','Mente Maker','container','txt');
